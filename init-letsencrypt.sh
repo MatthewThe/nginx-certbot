ee471ff27fadfd1edb1334f3ee81370f98a11d81
Matthew The <matthewthe@gmail.com>
2021-10-26 22:04:22 +0200

    Added matomo support

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(livelca.com www.livelca.com)
+domains=(livelca.com www.livelca.com matomo.livelca.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="matthew@livelca.com" # Adding a valid address is strongly recommended
